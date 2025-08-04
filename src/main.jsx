313abeb2710526d93094548b7fb8afaf8035ada2
An Jaebeom <ajb8533296@gmail.com>
2025-08-04 16:02:06 +0900

    project setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
+import { Provider } from "@/components/ui/provider"
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import './index.css'
-import App from './App.jsx'
+
+import Main from './App.jsx'
+
+// React Query 클라이언트 생성
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <Router>
+          <Routes>
+            {/* 메인 페이지 */}
+            <Route path="/" element={<Main />} />
+          </Routes>
+        </Router>
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>,
 )
