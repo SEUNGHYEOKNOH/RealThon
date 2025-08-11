1b8a6fae1e5310b96548c8b6615e1d560251e238
An Jaebeom <ajb8533296@gmail.com>
2025-08-11 22:49:43 +0900

    add choicephone and selectphone

---
+++ src/components/SelectPhone/SelectPhone.jsx
@@ -0,0 +1,107 @@
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
+
+import PhoneFrame from '@/assets/PhoneFrame.png';
+
+export default function SelectPhone() {
+    const phoneData = {
+        name: 'Galaxy S25',
+        model: 'BEST SHOT',
+        data: `줌
+광학줌 21배
+광학줌 21배
+초점거리
+f = 4.1~86.1 mm
+
+(35 mm filmequivalent : 23 ~ 483 mm)
+4.5-45.0 mm
+
+(35 mm 카메라 환산 25-250 mm 상당의 촬영 화각)
+손떨림 보정
+OIS (광학식 손떨림 보정)
+렌즈 시프트 방식과 전자식 병용(정지화상)
+렌지 시프트 방식(동영상)
+F No
+2.8(W) ~ 5.9 (T)
+f/3.5-5.8
+셔터 스피드
+Auto : 1/8 ~1/2,000
+secManual : 16 ~1/2,000sec
+1/2000 ~ 1초
+1/4000초 (고속연사시 최고 속도),
+4초 (장면 모드의 [불꽃놀이])`,
+    };
+
+    return (
+        <Flex
+            w="100vw"
+            h="100vh"
+
+            justify="center"
+            align="center"
+        >
+            <Flex>
+                <Box
+                    mr="-50px"
+                >
+                    <Image
+                        src={PhoneFrame}
+                        alt="Phone Frame"
+                        
+                        h="503px"
+                    />
+                </Box>
+                <Box
+                    py="20px"
+                >
+                    <Text
+                        fontSize="40px"
+                    >
+                        Galaxy S25
+                    </Text>
+                    <Text
+                        fontSize="20px"
+                    >
+                        BEST SHOT
+                    </Text>
+
+                    <Flex
+                        w="757px"
+                        h="350px"
+
+                        border="1px solid black"
+
+                        overflowY="scroll"
+                        
+                    >
+                        <Box
+                            w="50%"
+                        >
+                            <Text
+                                whiteSpace="pre-wrap"
+                                >
+                                {phoneData.data}
+                            </Text>
+                        </Box>
+                        <Box
+                            w="50%"
+                        >
+                            <Text
+                                whiteSpace="pre-wrap"
+                                >
+                                은하수 ㅇ내ㅓ
+                                <br />
+ㅇdsklnfkldnsl
+<br />
+fisdfiopsdj
+<br />
+fpsidjfpiosdj
+<br />
+sdfpiojpsodfjpo
+                            </Text>
+                        </Box>
+                    </Flex>
+                </Box>
+            </Flex>
+        </Flex>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from '@/components/ui/provider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import './index.css';
 
 import Main from './pages/Main/Main.jsx';
+import ChoicePhone from './components/ChoicePhone/ChoicePhone.jsx';
+import SelectPhone from './components/SelectPhone/SelectPhone';
 // import OtherPage from './pages/OtherPage.jsx';  // 주석처리 하세요! 파일이 없으면
 
 const queryClient = new QueryClient();
@@ -20,6 +23,10 @@ createRoot(document.getElementById('root')).render(
 
                         {/* <Route path="/other" element={<OtherPage />} /> */}
                         {/* OtherPage.jsx가 없으면 반드시 주석 또는 삭제 */}
+
+                        {/* For Test */}
+                        <Route path="/choice-phone" element={<ChoicePhone />} />
+                        <Route path="/select-phone" element={<SelectPhone />} />
                     </Routes>
                 </Router>
             </Provider>
